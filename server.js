07fd75b4eadef38947f6e0b4ebd968c5e9a3435b
andrepg1184 <andreleite1184@hotmail.com>
2023-01-27 11:54:14 -0300

    Att config

--- server.js
+++ server.js
@@ -5,20 +5,39 @@ import * as fs from 'fs';
 dotenv.config();
 
 const configuration = new Configuration({
-    organization: "org-ZuSCC8Twtsd6vyBZ1JtyLJbT",
+    organization: "org-hmSegK72n5o98pUVjhZoVlCI",
     apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const response = await openai.createImageEdit(
+
+// Editar imagem já existente
+
+/*const response = await openai.createImageEdit(
     fs.createReadStream("./images/IMG_2909.png"),
     fs.createReadStream("./images/IMG_2909_mask.png"),
-    "an apocalypse scenario with cyberpunk suits",
+    "Gerar imagem futuristica",
     1,
     "1024x1024"
   );
-  
+*/
+
+// Gerar imagem com linha de comando
+/*
+const response = await openai.createImage({
+    prompt: " gerar uma guerreira com cabelo preto",
+    n: 1,
+    size: "1024x1024",
+  });
+
+*/
+const response = await openai.createImageVariation(
+    fs.createReadStream("./images/12.png"),
+    1,
+    "1024x1024"
+  );
+
   let image_url;
   
   image_url = response.data.data[0].url;
